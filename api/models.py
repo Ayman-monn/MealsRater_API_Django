fbfbbbf976f7f1c5962c7fa104cb74d90baa0f44
Ayman-monn <ayman10112013@gmail.com>
2024-02-03 22:31:57 +0200

    BasicSetUp

---
+++ api/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class Meal(models.Model): 
+    title = models.CharField(max_length=250)
+    description = models.TextField() 
+
+    def __str__(self):
+        return self.title 
+    
+    
+class Rating(models.Model): 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    def __str__(self): 
+        return str(self.meal) 
+    
+
+
+    class Meta:
+        unique_together = (('user', 'meal'))
+        index_together = (('user', 'meal'))
+
+
+
---
+++ api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include 
+from rest_framework import routers 
+from .views import MealViewSet, RatingViewSet
+
+router = routers.DefaultRouter()
+router.register('maels',MealViewSet) 
+router.register('ratings', RatingViewSet) 
+
+
+urlpatterns = [
+    path('', include(router.urls))
+]
+
+
